6251c48af20f66837b3745075167986f23247b79
Malkovski <malkovski.georgi@gmail.com>
2014-12-23 00:42:50 +0200

    StyleCop-ed finished task.

--- C# Part 1/Exercises/PageGame/PageGame.cs
+++ C# Part 1/Exercises/PageGame/PageGame.cs
@@ -9,109 +9,250 @@ using System.Threading.Tasks;
    public class PageGame
     {
        public static void Main(string[] args)
-        {
-            int[,] cookieBox = new int[16, 16];
-
-            for (int row = 0; row < cookieBox.GetLength(0); row++)
-            {
-                for (int col = 0; col < cookieBox.GetLength(1); col++)
-                {
-                    cookieBox[row, col] = int.Parse(Console.ReadLine());
-                }
-            }
-
-            string question = Console.ReadLine();
-            int rowLoc = int.Parse(Console.ReadLine());
-            int colLoc = int.Parse(Console.ReadLine());
-            bool center = false;
+        {           
             bool cookie = false;
             bool crumb = false;
             bool brokenCookie = false;
-            decimal tax = 0;
+            decimal tax = 0.00M;           
+            int[,] cookieBox = new int[16, 16];
 
-            if ((rowLoc == 1) | (rowLoc == 4) | (rowLoc == 7) | (rowLoc == 10) |  (rowLoc == 13))
+            for (int row = 0; row < 16; row++)
             {
-                if ((colLoc == 1) | (colLoc == 4) | (colLoc == 7) | (colLoc == 10) | (colLoc == 13))
-                {
-                    center = true;
-                }
+                string cookieLine = Console.ReadLine();
+                        
+                    for (int col = 0; col < cookieLine.Length; col++)
+                    {
+                        cookieBox[row, col] = int.Parse(cookieLine[col].ToString());
+                    }           
             }
 
-            if (cookieBox[rowLoc, colLoc] == 1)
+            for (;;)
             {
-                if ((cookieBox[rowLoc - 1, colLoc - 1] == 1) & (cookieBox[rowLoc - 1, colLoc] == 1) &
-                 (cookieBox[rowLoc - 1, colLoc + 1] == 1) & (cookieBox[rowLoc, colLoc - 1] == 1) &
-                 (cookieBox[rowLoc, colLoc + 1] == 1) & (cookieBox[rowLoc + 1, colLoc - 1] == 1) &
-                 (cookieBox[rowLoc + 1, colLoc] == 1) & (cookieBox[rowLoc + 1, colLoc + 1] == 1))
+                string question = Console.ReadLine();
+                if (question == "paypal")
                 {
-                    cookie = true;
-                }
-                else if (((cookieBox[rowLoc - 1, colLoc - 1] == 0) & (cookieBox[rowLoc - 1, colLoc] == 0) &
-                 (cookieBox[rowLoc - 1, colLoc + 1] == 0) & (cookieBox[rowLoc, colLoc - 1] == 0) &
-                 (cookieBox[rowLoc, colLoc + 1] == 0) & (cookieBox[rowLoc + 1, colLoc - 1] == 0) &
-                 (cookieBox[rowLoc + 1, colLoc] ==01) & (cookieBox[rowLoc + 1, colLoc + 1] == 0)))
-                {
-                    crumb = true;
+                    Console.WriteLine("{0}", tax);
+                    break;
                 }
                 else
                 {
-                    brokenCookie = true;
-                }
-            }
-            else
-            {
-                if (((cookieBox[rowLoc - 1, colLoc - 1] == 1) | (cookieBox[rowLoc - 1, colLoc] == 1) |
-                 (cookieBox[rowLoc - 1, colLoc + 1] == 1) | (cookieBox[rowLoc, colLoc - 1] == 1) |
-                 (cookieBox[rowLoc, colLoc + 1] == 1) | (cookieBox[rowLoc + 1, colLoc - 1] == 1) |
-                 (cookieBox[rowLoc + 1, colLoc] == 1) | (cookieBox[rowLoc + 1, colLoc + 1] == 1)))
-	            {
-                    brokenCookie = true;
-	            }                 
-            }
-           // Page questions here...
-            switch (question)
-            {
-                case "what is":
-                    if (cookie == true)
-                    {
-                        Console.WriteLine("cookie");  
-                    }
-                    else if (brokenCookie == true)
-                    {
-                        Console.WriteLine("broken cookie"); 
-                    }
-                    else if (crumb == true)
-                    {
-                        Console.WriteLine("cookie crumb");  
-                    }
-                    break;
-                case "buy":
-                    if (cookie == true)
-                    {
-                       tax += 1.79M;
-                       cookieBox[rowLoc - 1, colLoc - 1] = 0;
-                       cookieBox[rowLoc - 1, colLoc] = 0;
-                       cookieBox[rowLoc - 1, colLoc + 1] = 0;
-                       cookieBox[rowLoc, colLoc - 1] = 0;
-                       cookieBox[rowLoc, colLoc + 1] = 0;
-                       cookieBox[rowLoc + 1, colLoc - 1] = 0;
-                       cookieBox[rowLoc + 1, colLoc] = 0;
-                       cookieBox[rowLoc + 1, colLoc + 1] = 0;  
-                    }
-                    else if ((brokenCookie == true) | (crumb == true))
-                    {
-                        Console.WriteLine("page"); 
-                    }
-                    else  
-                    {
-                        Console.WriteLine("smile");  
-                    }
-                    break;
+                    int rowLoc = int.Parse(Console.ReadLine());
+                    int colLoc = int.Parse(Console.ReadLine());
 
-                default:
-                    break;
-            }
-           
+                    if ((rowLoc != 0) & (colLoc != 0) & (rowLoc != 15) & (colLoc != 15))
+                    {
+                        if (cookieBox[rowLoc, colLoc] == 1)
+                        {
+                            if ((cookieBox[rowLoc - 1, colLoc - 1] == 1) & (cookieBox[rowLoc - 1, colLoc] == 1) &
+                             (cookieBox[rowLoc - 1, colLoc + 1] == 1) & (cookieBox[rowLoc, colLoc - 1] == 1) &
+                             (cookieBox[rowLoc, colLoc + 1] == 1) & (cookieBox[rowLoc + 1, colLoc - 1] == 1) &
+                             (cookieBox[rowLoc + 1, colLoc] == 1) & (cookieBox[rowLoc + 1, colLoc + 1] == 1))
+                            {
+                                cookie = true;
+                            }
+                            else if ((cookieBox[rowLoc - 1, colLoc - 1] == 0) & (cookieBox[rowLoc - 1, colLoc] == 0) &
+                             (cookieBox[rowLoc - 1, colLoc + 1] == 0) & (cookieBox[rowLoc, colLoc - 1] == 0) &
+                             (cookieBox[rowLoc, colLoc + 1] == 0) & (cookieBox[rowLoc + 1, colLoc - 1] == 0) &
+                             (cookieBox[rowLoc + 1, colLoc] == 0) & (cookieBox[rowLoc + 1, colLoc + 1] == 0))
+                            {
+                                crumb = true;
+                            }
+                            else
+                            {
+                                brokenCookie = true;
+                            }
+                        }
+                        else
+                        {
+                            cookie = false;
+                            crumb = false;
+                            brokenCookie = false;                          
+                        }
+                    }
+                    else
+                    {
+                        if (cookieBox[rowLoc, colLoc] == 1)
+                        {
+                            if (rowLoc == 0)
+                            {
+                                if ((colLoc > 0) & (colLoc < 15))
+                                {
+                                    if ((cookieBox[rowLoc, colLoc - 1] == 1) | (cookieBox[rowLoc, colLoc + 1] == 1))
+                                    {
+                                        brokenCookie = true;
+                                    }
+                                    else
+                                    {
+                                        crumb = true;
+                                    }
+                                }
+                                else
+                                {
+                                    if (colLoc == 0)
+                                    {
+                                        if ((cookieBox[rowLoc, colLoc + 1] == 1) | (cookieBox[rowLoc + 1, colLoc] == 1))
+                                        {
+                                            brokenCookie = true;
+                                        }
+                                        else
+                                        {
+                                            crumb = true;
+                                        }
+                                    }
+
+                                    if (colLoc == 15)
+                                    {
+                                        if ((cookieBox[rowLoc, colLoc - 1] == 1) | (cookieBox[rowLoc + 1, colLoc] == 1))
+                                        {
+                                            brokenCookie = true;
+                                        }
+                                        else
+                                        {
+                                            crumb = true;
+                                        }
+                                    }
+                                }
+                            }
+                            else if (rowLoc == 15)
+                            {
+                                if ((colLoc > 0) & (colLoc < 15))
+                                {
+                                    if ((cookieBox[rowLoc, colLoc - 1] == 1) | (cookieBox[rowLoc, colLoc + 1] == 1))
+                                    {
+                                        brokenCookie = true;
+                                    }
+                                    else
+                                    {
+                                        crumb = true;
+                                    }
+                                }
+                                else
+                                {
+                                    if (colLoc == 0)
+                                    {
+                                        if ((cookieBox[rowLoc, colLoc + 1] == 1) | (cookieBox[rowLoc - 1, colLoc] == 1))
+                                        {
+                                            brokenCookie = true;
+                                        }
+                                        else
+                                        {
+                                            crumb = true;
+                                        }
+                                    }
+
+                                    if (colLoc == 15)
+                                    {
+                                        if ((cookieBox[rowLoc, colLoc - 1] == 1) | (cookieBox[rowLoc - 1, colLoc] == 1))
+                                        {
+                                            brokenCookie = true;
+                                        }
+                                        else
+                                        {
+                                            crumb = true;
+                                        }
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                if (colLoc == 0)
+                                {
+                                    if (cookieBox[rowLoc, colLoc + 1] == 1)
+                                    {
+                                        brokenCookie = true;
+                                    }
+                                    else
+                                    {
+                                        crumb = true;
+                                    }
+                                }
+                                else
+                                {
+                                    if (colLoc == 15)
+                                    {
+                                        if (cookieBox[rowLoc, colLoc - 1] == 1)
+                                        {
+                                            brokenCookie = true;
+                                        }
+                                        else
+                                        {
+                                            crumb = true;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            cookie = false;
+                            crumb = false;
+                            brokenCookie = false;
+                        }
+                    }
+
+                   //// Page questions here...
+                    switch (question)
+                    {
+                        case "what is":
+                            if (cookie == true)
+                            {
+                                Console.WriteLine("cookie");
+                                cookie = false;
+                                crumb = false;
+                                brokenCookie = false;
+                            }
+                            else if (brokenCookie == true)
+                            {
+                                Console.WriteLine("broken cookie");
+                                cookie = false;
+                                crumb = false;
+                                brokenCookie = false;
+                            }
+                            else if (crumb == true)
+                            {
+                                Console.WriteLine("cookie crumb");
+                                cookie = false;
+                                crumb = false;
+                                brokenCookie = false;
+                            }
+                            else
+                            {
+                                Console.WriteLine("smile");
+                            }
+
+                            break;
+                        case "buy":
+                            if (cookie == true)
+                            {
+                                tax += 1.79M;
+                                cookieBox[rowLoc - 1, colLoc - 1] = 0;
+                                cookieBox[rowLoc - 1, colLoc] = 0;
+                                cookieBox[rowLoc - 1, colLoc + 1] = 0;
+                                cookieBox[rowLoc, colLoc - 1] = 0;
+                                cookieBox[rowLoc, colLoc] = 0;
+                                cookieBox[rowLoc, colLoc + 1] = 0;
+                                cookieBox[rowLoc + 1, colLoc - 1] = 0;
+                                cookieBox[rowLoc + 1, colLoc] = 0;
+                                cookieBox[rowLoc + 1, colLoc + 1] = 0;
+                                cookie = false;
+                            }
+                            else if ((brokenCookie == true) | (crumb == true))
+                            {
+                                Console.WriteLine("page");
+                                brokenCookie = false;
+                                crumb = false;
+                            }
+                            else
+                            {
+                                Console.WriteLine("smile");
+                            }
+
+                            break;
+                        default:
+                            break;
+                    } 
+                }              
+            }           
         }
     }
 }
