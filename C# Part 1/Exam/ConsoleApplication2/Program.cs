503b682834738917eaf60a34d34e35507dbb05e9
George Malkovski <malkovski.georgi@gmail.com>
2015-02-06 16:05:53 +0200

    C# Part One Exam

---
+++ C# Part 1/Exam/ConsoleApplication2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaddyKopper
+{
+    class SaddyKopper
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int counter = number.Length - 1;
+            BigInteger result = 0;
+            BigInteger newResult = 1;         
+            int transforms = 0;
+
+
+            if (transforms < 10)
+            {
+                while (counter > 0)
+                {
+                    number = number.Substring(0, number.Length - 1);
+
+                    for (int i = 0; i < number.Length; i++)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            result += int.Parse(number[i].ToString());
+                        }
+                    }
+
+                    
+                    result = result * newResult;
+                    newResult = result;
+                    result = 0;
+                    counter--;
+
+
+                    if (counter == 0)
+                    {
+                        transforms++;
+
+                        if (transforms == 10)
+                        {
+                            Console.WriteLine(newResult);
+                            break;
+                        }
+
+                        number = newResult.ToString();
+                        newResult = 1;
+
+                        if (number.Length == 1)
+                        {
+                            Console.WriteLine(transforms);
+                            Console.WriteLine(number);
+                            break;
+                        }
+                        else
+                        {
+                            counter = number.Length - 1;
+                            continue;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
