65087ec9a698d1217a5f5b6370be5ec30849d23d
Malkovski <malkovski.georgi@gmail.com>
2015-02-08 13:31:13 +0200

    Addig some more exercises with arrays!

---
+++ C# Part 2/MultiDimArraysHomeworks/MultidimArraysHomeworks/Task4BinSearch/BinSearch.cs
@@ -0,0 +1,37 @@
+﻿namespace Task4BinSearch
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+   public class BinSearch
+    {
+       public static void Main(string[] args)
+        {
+            int[] givenArr = { 3, 4, 66, 23, 14, 43, 8, 9, 5, 67, 45, 68, 1, 2, 5, 10 };
+            Array.Sort(givenArr);
+            int number = int.Parse(Console.ReadLine());
+
+            int myIndex = Array.BinarySearch(givenArr, number);
+
+            if (myIndex < 0)
+            {
+                while (myIndex < 0)
+                {
+                    number--;
+                    myIndex = FindTheIndex(givenArr, number);
+                }
+            }
+            
+            Console.WriteLine("Result: {0}", givenArr[myIndex]);
+        }
+
+       private static int FindTheIndex(int[] givenArr, int number)
+       {
+           int myIndex = Array.BinarySearch(givenArr, number);
+           return myIndex;
+       }
+    }
+}
---
+++ C# Part 2/MultiDimArraysHomeworks/MultidimArraysHomeworks/Task5SortByStrLength/SortByStrLentgh.cs
@@ -0,0 +1,67 @@
+﻿namespace Task5SortByStrLength
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+   public class SortByStrLentgh
+    {
+       public static void Main(string[] args)
+        {
+            string[] givenArr = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+            int[] itemLen = new int[givenArr.Length];
+            string[] sorted = new string[givenArr.Length];
+
+            for (int i = 0; i < givenArr.Length; i++)
+            {
+                itemLen[i] = givenArr[i].Length;
+            }
+
+            int size = 1;
+            int cnt = 0;
+            int elements = sorted.Length;
+
+            while (elements > 0)
+            {
+                int myIndex = FindTheIndex(itemLen, size);
+
+                if (myIndex >= 0)
+                {
+                    sorted[cnt] = givenArr[myIndex];
+                    itemLen[myIndex] = Int32.MaxValue;
+                    cnt++;
+                    elements--;
+                }
+                else
+                {
+                    size++;
+                }
+            }
+
+            foreach (var item in sorted)
+            {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+
+            Console.WriteLine();
+        }
+
+       private static int FindTheIndex(int[] itemLen, int size)
+       {
+           int myIndex = -1;
+
+           for (int i = 0; i < itemLen.Length; i++)
+           {
+               if (itemLen[i] == size)
+               {
+                   myIndex = i;
+               }           
+           }
+
+           return myIndex;
+       }
+    }
+}
