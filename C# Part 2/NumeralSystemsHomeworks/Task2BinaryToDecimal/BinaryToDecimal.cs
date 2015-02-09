4e7b28e04ce02a118eaf0edfc586325e2eb48751
George Malkovski <malkovski.georgi@gmail.com>
2015-02-09 14:23:20 +0200

    Making the code little more client friendly

--- C# Part 2/NumeralSystemsHomeworks/Task2BinaryToDecimal/BinaryToDecimal.cs
+++ C# Part 2/NumeralSystemsHomeworks/Task2BinaryToDecimal/BinaryToDecimal.cs
@@ -1,26 +1,27 @@
 ﻿namespace Task2BinaryToDecimal
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
 
    public class BinaryToDecimal
-    {
-       public static void Main(string[] args)
-        {
-            string binar = Console.ReadLine();
-            int result = 0;
-            int cnt = 0;
+	{
+	   public static void Main(string[] args)
+		{
+			Console.WriteLine("Enter binary number!");
+			string binar = Console.ReadLine();
+			int result = 0;
+			int cnt = 0;
 
-            for (int i = binar.Length - 1; i >= 0; i--)
-            {
-                result += int.Parse(binar[i].ToString()) * (int)(Math.Pow(2, cnt));
-                cnt++;
-            }
+			for (int i = binar.Length - 1; i >= 0; i--)
+			{
+				result += int.Parse(binar[i].ToString()) * (int)(Math.Pow(2, cnt));
+				cnt++;
+			}
 
-            Console.WriteLine("Decimal is: {0}", result);
-        }
-    }
+			Console.WriteLine("Decimal is: {0}", result);
+		}
+	}
 }
