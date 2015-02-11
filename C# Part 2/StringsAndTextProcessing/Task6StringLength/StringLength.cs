1f5326092547a6fcd9bff8f8b4c30b4254e0f976
Malkovski <malkovski.georgi@gmail.com>
2015-02-11 20:57:33 +0200

    Replaced folders

---
+++ C# Part 2/StringsAndTextProcessing/Task6StringLength/StringLength.cs
@@ -0,0 +1,43 @@
+﻿namespace Task6StringLength
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+   public class StringLength
+    {
+       public static void Main(string[] args)
+        {
+            StringBuilder word = new StringBuilder();
+            ConsoleKeyInfo stop;
+
+            for (int i = 0; i < 20; i++)
+            {
+                stop = Console.ReadKey();
+                
+                if (stop.Key != ConsoleKey.Enter)
+                {                  
+                    word.Append(stop.KeyChar);
+                }
+                else
+                {
+                    break;
+                }       
+            }
+
+            Console.WriteLine();
+                    
+            if (word.Length < 20)
+            {
+                while (word.Length < 20)
+                {
+                    word.Append("*");
+                }
+            }
+
+            Console.WriteLine(word);
+        }
+    }
+}
