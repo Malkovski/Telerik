db9d6592a81e5cea38699d3714a81623488f62a0
Malkovski <malkovski.georgi@gmail.com>
2015-03-05 19:57:06 +0200

    Added templates for use in future

---
+++ C# Part 2/ExamTemplates/DynamicOpt-Subsequence/Subsequence.cs
@@ -0,0 +1,55 @@
+﻿namespace DynamicOpt_Subsequence
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+   public class Subsequence
+    {
+       public static void Main(string[] args)
+        {
+            
+           int m = int.Parse(Console.ReadLine());
+           int n = int.Parse(Console.ReadLine());
+           string[] firstLine = new string[m];
+           string[] secondLine = new string[n];
+           firstLine = Console.ReadLine().Split(' ');
+           secondLine = Console.ReadLine().Split(' ');
+
+           string result = FindSubsequence(firstLine, secondLine, m , n);
+           Console.WriteLine(result);
+
+        }
+
+       private static string FindSubsequence(string[] firstLine, string[] secondLine, int m, int n)
+       {
+          int[,] grid = new int[m + 1, n + 1];
+
+           for (int i = 0; i < m + 1; i++)
+           {
+               for (int j = 0; j < n + 1; j++)
+               {
+                   if (i == 0 || j == 0)
+                   {
+                       grid[i, j] = 0;
+                   }
+                   else
+                   {
+                       if (firstLine[i - 1] == secondLine[j - 1])
+                       {
+                           grid[i, j] = grid[i - 1, j - 1] + 1;
+                       }
+                       else
+                       {
+                           grid[i, j] = Math.Max(grid[i - 1, j], grid[i, j - 1]);
+                       }
+                   }   
+               }
+           }
+
+           return grid[m, n].ToString();
+       }
+    }
+}
---
+++ C# Part 2/ExamTemplates/DynamicOpt-Trees/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace DynamicOpt_Trees
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+   public class Program
+    {
+       public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());         
+            int start = -1;
+            int[] num = new int[] { a, b, c, d, start };
+            int count = 0;
+            int[] numList = new int[a + b + c + d];
+            List<int> list = new List<int>();
+
+                for (int i = 0; i < num.Length; i++)
+                {
+                    for (int j = 0; j < num[i]; j++)
+                    {
+                        list.Add(i);
+                    }
+                }
+                numList = list.ToArray();
+                Array.Sort(numList);
+
+            do
+	        {
+                if (IsValid(numList))
+                {
+                    ++count;
+                }
+	        } 
+            while (NextPermutation(numList));
+ 
+            Console.WriteLine(count);
+        }
+
+       static bool IsValid(int[] numList)
+       {
+           for (int i = 1; i < numList.Length; i++)
+           {
+               if (numList[i - 1] == numList[i])
+               {
+                   return false;
+               }
+           }
+
+           return true;
+       }
+
+       /// Thanks by: http://stackoverflow.com/questions/11208446/generating-permutations-of-a-set-most-efficiently ////
+       
+       private static bool NextPermutation(int[] numList)
+       {
+           /*
+            Knuths
+            1. Find the largest index j such that a[j] < a[j + 1]. If no such index exists, the permutation is the last permutation.
+            2. Find the largest index l such that a[j] < a[l]. Since j + 1 is such an index, l is well defined and satisfies j < l.
+            3. Swap a[j] with a[l].
+            4. Reverse the sequence from a[j + 1] up to and including the final element a[n].
+
+            */
+           var largestIndex = -1;
+           for (var i = numList.Length - 2; i >= 0; i--)
+           {
+               if (numList[i] < numList[i + 1])
+               {
+                   largestIndex = i;
+                   break;
+               }
+           }
+
+           if (largestIndex < 0) return false;
+
+           var largestIndex2 = -1;
+           for (var i = numList.Length - 1; i >= 0; i--)
+           {
+               if (numList[largestIndex] < numList[i])
+               {
+                   largestIndex2 = i;
+                   break;
+               }
+           }
+
+           var tmp = numList[largestIndex];
+           numList[largestIndex] = numList[largestIndex2];
+           numList[largestIndex2] = tmp;
+
+           for (int i = largestIndex + 1, j = numList.Length - 1; i < j; i++, j--)
+           {
+               tmp = numList[i];
+               numList[i] = numList[j];
+               numList[j] = tmp;
+           }
+
+           return true;
+       }
+    }
+}
