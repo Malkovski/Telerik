17d823320ca3ab7d68c4a8132be452fe93b1f082
George Malkovski <malkovski.georgi@gmail.com>
2015-03-25 17:53:08 +0200

    Adding task with exceptions

---
+++ OOP/Homework/RangeException/InvalidRangeException.cs
@@ -0,0 +1,24 @@
+﻿namespace RangeException
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class InvalidRangeException<T> : ApplicationException
+    {
+        public InvalidRangeException(string message, T value, T minValue, T maxValue)
+            : base(message)
+        {
+            this.Value = value;
+            this.MinValue = minValue;
+            this.MaxValue = maxValue;
+        }
+
+        public T Value { get; private set; }
+        public T MinValue { get; private set; }
+        public T MaxValue { get; private set; }
+
+    }
+}
---
+++ OOP/Homework/RangeException/RangeApplicationSystem.cs
@@ -0,0 +1,37 @@
+﻿namespace RangeException
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class RangeApplicationSystem
+    {
+        public static void Main()
+        {
+            int num = 0;
+            int minNum = 1000;
+            int maxNum = 2000;
+
+            try
+            {
+                num = int.Parse(Console.ReadLine());
+
+                if (num < minNum || num > maxNum)
+                {
+                    throw new InvalidRangeException<int>("Not in range!!!", num, minNum, maxNum);
+                }
+            }
+            catch (InvalidRangeException<int> ex)
+            {
+
+                Console.WriteLine("InvalidRangeException: Number {0} must be in the range {1} - {2}", ex.Value, ex.MinValue, ex.MaxValue);
+            }
+            catch (ApplicationException)
+            {
+                Console.WriteLine("Operation failed!!!");
+            }
+        }
+    }
+}
