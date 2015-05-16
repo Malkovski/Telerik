03f8a688786c4045114e45021a7df5abc209b638
George Malkovski <malkovski.georgi@gmail.com>
2015-05-16 18:49:14 +0300

    Initial JavaScript folder commit.

---
+++ JavaScriptFundamentials/PracticcingJS/TastOne/TastOne/scripts/script.js
@@ -0,0 +1,23 @@
+﻿function clickMe() {
+    alert('This is my first JS!');
+}
+
+function promptMe() {
+    prompt('Do you understand this JS?');
+}
+
+function askMe() {
+    confirm('Can you imagine this?')
+}
+
+var a = 50880;
+var b = 234;
+var c = a - b;
+console.log('The result is:' + c);
+
+function showResult() {
+    alert('The result is: ' + c);
+}
+
+var p = undefined;
+console.log(p);
