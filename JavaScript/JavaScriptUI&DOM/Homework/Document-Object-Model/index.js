19c406dad603094996120112d0ff4921c384efca
George Malkovski <malkovski.georgi@gmail.com>
2015-07-15 18:50:22 +0300

    DOM homework - finished task 1,2,3

--- JavaScript/JavaScriptUI&DOM/Homework/Document-Object-Model/index.js
+++ JavaScript/JavaScriptUI&DOM/Homework/Document-Object-Model/index.js
@@ -1,3 +1,8 @@
+/*Write a script that selects all the div nodes that are directly inside other div elements
+Create a function using querySelectorAll()
+Create a function using getElementsByTagName()*/
+
+
 function showCountOfNestedDivsWithQuerry() {
     var allDivsInDivs = document.querySelectorAll('div div');
     var p = document.getElementsByTagName('p')[0];
@@ -23,4 +28,20 @@ function showCountOfNestedDivsByTag() {
     var p = document.getElementById('by-tag-name');
     p.innerText = 'result with getElementsByTagName:';
     p.innerText += ' ' + nestedDivCount;
+}
+
+/*Create a function that gets the value of <input type="text"> ands prints its value to the console*/
+
+function showInputContent() {
+    var inputTextValue = document.querySelector('input').value;
+    console.log(inputTextValue);
+    document.getElementById('input-text-result').innerText += inputTextValue;
+}
+
+/* Cråate a function that gets the value of <input type="color"> and sets the background of the body to this value
+ */
+
+function changeBackgroundColor() {
+    var desiredColor = document.querySelectorAll('input')[1].value;
+    document.body.style.backgroundColor = desiredColor;
 }
