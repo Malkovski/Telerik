e22073f10cb9ae851713c29d822d1c4789429922
Malkovski <malkovski.georgi@gmail.com>
2015-07-15 20:10:51 +0300

    HQC homework added

---
+++ High-QualityCode/Homework/High-QualityMethods/Task 1.QualityMethods/Task 1.QualityMethods/Student.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_1.QualityMethods
+{
+    using System;
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string OtherInfo { get; set; }
+
+        public bool IsOlderThan(Student otherStudent)
+        {
+            DateTime firstDateOfBirth = this.GetDateOfBirth(this);
+            DateTime secondDateOfBirth = this.GetDateOfBirth(otherStudent);
+
+            return firstDateOfBirth < secondDateOfBirth;
+        }
+
+        private DateTime GetDateOfBirth(Student student)
+        {
+            return DateTime.Parse(student.OtherInfo.Substring(student.OtherInfo.Length - 10));
+        }
+    }
+}
