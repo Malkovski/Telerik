842f7f82f74c9ab5ed92e28b682f98e097bec174
Malkovski <malkovski.georgi@gmail.com>
2015-07-31 20:44:49 +0300

    Working on homework jQuery plugins

--- JavaScript/JavaScriptUI&DOM/Homework/jQueryPlugins/tasks/task-1.js
+++ JavaScript/JavaScriptUI&DOM/Homework/jQueryPlugins/tasks/task-1.js
@@ -2,10 +2,10 @@ function solve(){
     return function(selector){
         var $select,
             $options,
-            $div = $('<div/>'),
             $mainDiv,
             $currentDiv,
-            $containerDiv;
+            $containerDiv,
+            $div = $('<div/>');
 
         if (typeof selector === 'string') {
             if (selector === 'select') {
@@ -16,18 +16,39 @@ function solve(){
             }
         }
 
+        $select.css('display', 'none');
         $options = $select.find('option');
         $mainDiv = $div.clone().addClass('dropdown-list');
         $currentDiv = $div.clone().addClass('current');
         $mainDiv.append($currentDiv);
         $mainDiv.prepend($select);
+        $('body').append($mainDiv);
         $containerDiv = $div.clone().addClass('options-container').css('position', 'absolute').css('display', 'none');
         $mainDiv.append($containerDiv);
 
-        $options.each(function (option) {
-            var $currentOption = $(option);
 
+        for (var i = 0, len = $options.length; i < len; i++) {
+            var $currentOption = $options[i],
+                $divOption = $div.clone().addClass('dropdown-option').attr('data-value', $currentOption.value())
+                    .attr('data-index', i).html($currentOption.html());
+            $containerDiv.append($divOption);
+        }
+
+        $mainDiv.on('click', '.current', function () {
+            $containerDiv.css('display') === 'none' ?
+                ($containerDiv.show(),
+                $currentDiv.html('Select a value')) :
+                ($containerDiv.hide())
         });
+
+        $mainDiv.on('click', '.dropdown-item', function () {
+           var $this = $(this);
+            $currentDiv.html($this.html()).attr('data-value', $this.value());
+            $select.value($this.attr('data-value'));
+            $containerDiv.hide();
+        });
+
+
     };
 }
 
