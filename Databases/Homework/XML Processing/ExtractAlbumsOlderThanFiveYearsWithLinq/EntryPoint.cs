8cfc508cfaa2bce8ff15676f9118f867b5926f9a
Malkovski <malkovski.georgi@gmail.com>
2015-10-05 21:45:40 +0300

    Finishing homework about XML processing in .NET

--- Databases/Homework/XML Processing/ExtractAlbumsOlderThanFiveYearsWithLinq/EntryPoint.cs
+++ Databases/Homework/XML Processing/ExtractAlbumsOlderThanFiveYearsWithLinq/EntryPoint.cs
@@ -1,29 +1,27 @@
 ﻿namespace ExtractAlbumsOlderThanFiveYearsWithLinq
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Xml;
     using System.Xml.Linq;
 
-    class EntryPoint
+    internal class EntryPoint
     {
-        static void Main()
+        private static void Main()
         {
             XDocument doc = XDocument.Load("../../catalog.xml");
 
-            var albums = doc.Descendants("album");
+            var albums = from album in doc.Descendants("album")
+                                       where (int.Parse(album.Element("year").Value) <= (DateTime.Now.Year - 5))
+                                       select new
+                                       {
+                                           AlbumName = album.Element("name").Value,
+                                           AlbumPrice = album.Element("price").Value
+                                       };
 
             foreach (var album in albums)
             {
-                var year = album.Descendants("year").Take(1);
-                var price = album.Descendants("price");
-
-                Console.WriteLine(year);
+                Console.WriteLine("The album {0} is older than five years. Costs ${1}", album.AlbumName, album.AlbumPrice);
             }
-            
         }
     }
 }
