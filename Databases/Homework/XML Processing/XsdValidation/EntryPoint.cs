3d63f612db614bf4ae0784fc1bdb05b0881a25c5
Malkovski <malkovski.georgi@gmail.com>
2015-10-10 08:48:30 +0300

    phonebook problem practicing

---
+++ Databases/Homework/XML Processing/XsdValidation/EntryPoint.cs
@@ -0,0 +1,55 @@
+﻿namespace XsdValidation
+{
+    using System;
+    using System.IO;
+    using System.Xml;
+    using System.Xml.Schema;
+
+    public class EntryPoint
+    {
+        private const string XsdInvalidFileName = "../../InvalidCatalog.xsd";
+        private const string XmlInvalidFileName = "../../InvalidCatalog.xml";
+        private const string XsdFileName = "../../catalog.xsd";
+        private const string XmlFileName = "../../catalog.xml";
+        private const string SuccessMessage = "The document {0} validated against {1} is valid.";
+        private const string FailMessage = "The document {0} validated against {1} is NOT valid.";
+
+        private static bool isValid = true;
+
+        static void Main()
+        {
+            Validate(XsdFileName, XmlFileName);
+            Validate(XsdInvalidFileName, XmlInvalidFileName);
+        }
+
+        private static void Validate(string xsdFile, string xmlFile)
+        {
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.Schemas.Add(null, xsdFile);
+            settings.ValidationType = ValidationType.Schema;
+            settings.ValidationEventHandler += Handler;
+
+            XmlDocument document = new XmlDocument();
+            document.Load(xmlFile);
+            XmlReader reader = XmlReader.Create(new StringReader(document.InnerXml), settings);
+
+            while (reader.Read())
+            {
+            }
+
+            if (isValid)
+            {
+                Console.WriteLine(SuccessMessage, xmlFile, xsdFile);
+            }
+            else
+            {
+                Console.WriteLine(FailMessage, xmlFile, xsdFile);
+            }
+        }
+
+        private static void Handler(object sender, ValidationEventArgs e)
+        {
+            isValid = false;
+        }
+    }
+}
---
+++ Databases/Homework/XML Processing/XslTransformation/XslTransformEntryPoint.cs
@@ -0,0 +1,23 @@
+﻿namespace XslTransformation
+{
+    using System;
+    using System.Linq;
+    using System.Xml.Xsl;
+
+    internal class XslTransformEntryPoint
+    {
+        public class EntryPoint
+        {
+            private const string XmlFilename = "../../catalogue.xml";
+            private const string XslFilename = "../../catalogue.xsl";
+            private const string HtmlFilename = "../../catalogue.html";
+
+            public static void Main()
+            {
+                var xslt = new XslCompiledTransform();
+                xslt.Load(XslFilename);
+                xslt.Transform(XmlFilename, HtmlFilename);
+            }
+        }
+    }
+}
