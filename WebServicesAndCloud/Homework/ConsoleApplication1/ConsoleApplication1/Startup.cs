c1b876af70529f2055c1a434f29299c838451452
George Malkovski <malkovski.georgi@gmail.com>
2015-11-06 18:48:12 +0200

    homeworkss

---
+++ WebServicesAndCloud/Homework/ConsoleApplication1/ConsoleApplication1/Startup.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApplication1
+{
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+
+    public class Startup
+    {
+        private const string Url = @"http://api.feedzilla.com";
+
+        static void Main()
+        {
+            var webClient = new WebClient();
+            ConsumePublicApi(webClient, Url, "/v1/categories.json");
+
+        }
+
+        static void ConsumePublicApi(WebClient client, string address, string query)
+        {
+            var result = client.DownloadString(address + query);
+
+            var response = JsonConvert.DeserializeObject(result);
+
+
+
+        }
+    }
+}
