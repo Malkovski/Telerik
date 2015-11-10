9a34088f332a46ddc4d245a98a36b20af23554d1
Malkovski <malkovski.georgi@gmail.com>
2015-11-10 22:10:37 +0200

    DSA practicing

---
+++ DSA/Homework/Workshop1/JediMeditation/JediMeditationWithBag/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace JediMeditationWithBag
+{
+    using System;
+    using System.Linq;
+    using Wintellect.PowerCollections;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            string[] jedis = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            var orderedByRank = new OrderedBag<string>(jedis, RankComparer);
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(orderedByRank[i]);
+
+                Console.Write(" ");
+            }
+        }
+
+        private static int RankComparer(string firstRank, string secondRank)
+        {
+            int firstPriority = GetPriority(firstRank);
+            int secondPriority = GetPriority(secondRank);
+
+            return firstPriority.CompareTo(secondPriority);
+        }
+
+        private static int GetPriority(string element)
+        {
+            int priority = -1;
+
+            switch (element[0].ToString().ToLower())
+            {
+                case "m":
+                    priority = 0;
+                    break;
+                case "k":
+                    priority = 1;
+                    break;
+                case "p":
+                    priority = 2;
+                    break;
+            }
+
+            return priority;
+        }
+    }
+}
---
+++ DSA/Homework/Workshop1/JediMeditation/SimpleJediMeditation/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace SimpleJediMeditation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main()
+        {
+            var masters = new List<string>();
+            var knights = new List<string>();
+            var padwans = new List<string>();
+
+            var n = int.Parse(Console.ReadLine());
+
+            var jedis = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < n; i++)
+            {
+                if (jedis[i].StartsWith("m"))
+                {
+                    masters.Add(jedis[i]);
+                }
+
+                if (jedis[i].StartsWith("k"))
+                {
+                    knights.Add(jedis[i]);
+                }
+
+                if (jedis[i].StartsWith("p"))
+                {
+                    padwans.Add(jedis[i]);
+                }
+            }
+
+            foreach (var item in masters)
+            {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+
+            foreach (var item in knights)
+            {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+
+            foreach (var item in padwans)
+            {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+        }
+    }
+}
