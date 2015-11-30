e8d46c2afd8a7a9a28ea0cad606487fb54ca0729
George Malkovski <malkovski.georgi@gmail.com>
2015-11-30 18:34:25 +0200

    Started Portals task solution

---
+++ DSA/Practices/DSA2014Exam/DSA2014/Portals/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portals
+{
+    class Program
+    {
+        private static HashSet<int> results = new HashSet<int>();
+        private static string[,] matrix;
+        private static int top;
+
+        static void Main(string[] args)
+        {
+            var startPos = Console.ReadLine().Split(' ');
+            var row = int.Parse(startPos[0]);
+            var col = int.Parse(startPos[1]);
+
+            var size = Console.ReadLine().Split(' ');
+            var rows = int.Parse(size[0]);
+            var cols = int.Parse(size[1]);
+
+            matrix = new string[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                var data = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = data[j];
+                }
+            }
+
+            Teleport(row, col);
+            Console.WriteLine(results.First());
+        }
+
+        public static void Teleport(int row, int col)
+        {
+            var current = int.Parse(matrix[row, col]);
+            matrix[row, col] = "#";
+
+            top += current;
+            results.Add(top);
+
+            if (row - current >= 0 && matrix[row - current, col] != "#")
+            {
+                Teleport(row - current, col);
+            }
+
+            if (row + current <= matrix.GetLength(0) - 1 && matrix[row + current, col] != "#")
+            {
+                Teleport(row + current, col);
+            }
+
+            if (col + current <= matrix.GetLength(1) - 1 && matrix[row, col + current] != "#")
+            {
+                Teleport(row, + col + current);
+            }
+
+            if (col - current >= 0 && matrix[row, col - current] != "#")
+            {
+                Teleport(row, + col - current);
+            }
+        }
+    }
+}
---
+++ DSA/Testing/ShortestPath/ShortestPath/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortestPath
+{
+    class Program
+    {
+        static string letters = "LSR";
+        static string input = Console.ReadLine();
+        static int  n = input.Length;
+
+        static void Main(string[] args)
+        {
+
+            var parts = input.Split('*');
+            
+
+            // S * L * R * S
+
+            Build(0);
+        }
+
+        public static void Build(int index)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                foreach (var item in letters)
+                {
+                    if (input[i] != '*')
+                    {
+                        Console.Write(input[i]);
+                    }
+                    else
+                    {
+                        for (int j = i; j < n; j++)
+                        {
+                            foreach (var el in letters)
+                            {
+                                if (input[j] != '*')
+                                {
+                                    Console.Write(input[j]);
+                                }
+                                else
+                                {
+                                    for (int k = j; k < n; k++)
+                                    {
+                                        foreach (var tt in letters)
+                                        {
+                                            if (input[k] != '*')
+                                            {
+                                                Console.Write(input[k]);
+                                            }
+                                            else
+                                            {
+                                                Console.Write(tt);
+                                            }
+                                        }
+                                    }
+                                }
+                                Console.Write(el);
+                            }
+
+                            
+                        }
+
+                        
+                    }
+                    Console.Write(item);
+                }
+            }
+        }
+    }
+}
